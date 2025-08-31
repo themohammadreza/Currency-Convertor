eed286677fbd35001bd94c9b6daf6ad0fd09aa6f
Mohammadreza Naseri <themohammadreza2006@gmail.com>
2025-08-31 21:01:04 +0330

    Create Project Structure

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import requests
+
+def get_exchange_rate(src_currency, dst_currency):
+    pass
+
+def convert_currency(amount, src_currency, dst_currency):
+    pass
+
+if __name__ == "__main__":
+    pass
